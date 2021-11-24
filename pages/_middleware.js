0e74bfd69b96917351566410be05ad7bc68fd1a6
SankhaChak <anonproductionspro@gmail.com>
2021-11-25 03:55:13 +0530

    checking headers

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(req, ev) {
-  return req.headers["x-forwarded-proto"] !== "http"
-    ? NextResponse.redirect(`http://${req.nextUrl.hostname}`)
-    : NextResponse.next();
+  console.log(req.headers);
+
+  // return req.headers["x-forwarded-proto"] !== "http"
+  //   ? NextResponse.redirect(`http://${req.nextUrl.hostname}`)
+  //   : NextResponse.next();
+
+  return NextResponse.next();
 }
