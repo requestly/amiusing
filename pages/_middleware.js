807fc76fccf5a43ed1f5334ac788533571601b90
SankhaChak <anonproductionspro@gmail.com>
2021-11-25 03:59:54 +0530

    changed http to https

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,11 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(req, ev) {
-  console.log(req.headers);
-
-  // return req.headers["x-forwarded-proto"] !== "http"
-  //   ? NextResponse.redirect(`http://${req.nextUrl.hostname}`)
-  //   : NextResponse.next();
-
-  return NextResponse.next();
+  return req.headers["x-forwarded-proto"] !== "https"
+    ? NextResponse.redirect(`http://${req.nextUrl.hostname}`)
+    : NextResponse.next();
 }
