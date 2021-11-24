23b362321106d372a8f17047fb7c334a7fe42af6
SankhaChak <anonproductionspro@gmail.com>
2021-11-25 03:49:14 +0530

    modified middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,13 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(req, ev) {
-  console.log({
-    hn: req.nextUrl.hostname,
-    o: req.nextUrl.origin,
-    nU: req.nextUrl,
-  });
-  // return req.headers["x-forwarded-proto"] !== "http"
-  //   ? NextResponse.redirect(`http://${req.hostname}${req.originalUrl}`)
-  //   : NextResponse.next();
-  return NextResponse.next();
+  return req.headers["x-forwarded-proto"] !== "http"
+    ? NextResponse.redirect(`http://${req.nextUrl.hostname}`)
+    : NextResponse.next();
 }
