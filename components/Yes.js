26c70064bc27c9abddae4f97dd3d3f92406aaf17
Somil Gupta <gsomil93@gmail.com>
2025-10-10 11:48:41 +0530

    reverting back the links and testing as well, all working

--- components/Yes.js
+++ components/Yes.js
@@ -33,56 +33,56 @@ export default function Yes() {
 
         <div className="options">
           <a
-            href="https://docs.requestly.io/getting-started/installing-requestly"
+            href="https://docs.requestly.com/general/getting-started--introduction"
             target="_blank"
             rel="noreferrer noopener"
           >
             Getting started
           </a>
           <a
-            href="https://docs.requestly.io/http-rules/rule-operators"
+            href="https://docs.requestly.com/general/advanced-usage/rule-operators"
             target="_blank"
             rel="noreferrer noopener"
           >
             Understanding Rule Operators
           </a>
           <a
-            href="https://docs.requestly.io/http-rules/modify-headers"
+            href="https://docs.requestly.com/general/rule-types/modify-headers"
             target="_blank"
             rel="noreferrer noopener"
           >
             Modify HTTP(s) Headers
           </a>
           <a
-            href="https://docs.requestly.io/http-rules/modify-response"
+            href="https://docs.requestly.com/general/http-rules--rule-types/modify-response-body"
             target="_blank"
             rel="noreferrer noopener"
           >
             Modify Response Rule
           </a>
           <a
-            href="https://docs.requestly.io/http-rules/insert-custom-script"
+            href="https://docs.requestly.com/general/http-rules--rule-types/modify-dominject-scripts"
             target="_blank"
             rel="noreferrer noopener"
           >
             Inject Javascript or CSS to any website
           </a>
           <a
-            href="https://docs.requestly.io/http-rules/replace-rule"
+            href="https://docs.requestly.com/general/rule-types/replace-strings"
             target="_blank"
             rel="noreferrer noopener"
           >
             Conditionally replace part of an URL String
           </a>
           <a
-            href="https://docs.requestly.io/http-rules/modify-user-agent"
+            href="https://docs.requestly.com/general/rule-types/modify-user-agents"
             target="_blank"
             rel="noreferrer noopener"
           >
             Modify User Agent
           </a>
           <a
-            href="https://docs.requestly.io/getting-started/sharing-rules"
+            href="https://docs.requestly.com/general/others/sharing-rules"
             target="_blank"
             rel="noreferrer noopener"
           >
